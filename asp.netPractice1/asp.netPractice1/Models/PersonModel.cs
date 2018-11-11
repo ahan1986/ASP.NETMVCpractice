150819a53a09e410905e38d8f887c72a8405b522
Handrew <andrew.a.han@gmail.com>
2018-11-10 21:08:30 -0500

    create model for a person

---
+++ asp.netPractice1/asp.netPractice1/Models/PersonModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace asp.netPractice1.Models
+{
+    public class PersonModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; } = 0;
+        public bool IsAlive { get; set; } = true;
+    }
+}
